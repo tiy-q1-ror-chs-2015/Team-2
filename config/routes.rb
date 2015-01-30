5b7789085b9dec4b30571e21ce01210a8a50e95b
Robbi Kenney <robbikenney@gmail.com>
2015-01-30 15:12:28 -0500

    basic discovery page is working but we need to get new post data to populate the db

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   get 'discover' => 'discover#index'
+  post 'discover_create_post' => 'discover#create_post'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
