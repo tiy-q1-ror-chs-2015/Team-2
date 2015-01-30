2aa474268fb22d2fd8b96af9ee71a43a1351e7c6
Travis Nesland <tnesland@gmail.com>
2015-01-29 23:56:22 -0500

    Get user authentication working.
    Implemented user authentication and testing with a simple home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,13 @@
 class HomeController < ApplicationController
+
+  def index
+    # if the user is logged in, redirect to the current user page,
+    # otherwise render our marketing page
+    if user_signed_in?
+      @informative_text = "#{current_user.email} is signed in"
+    else
+      @informative_text = 'No user is singed in'
+    end
+  end
+
 end
